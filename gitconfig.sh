b871b8d4ea5d88898809b6ccd48d331d97e970bc
Ryan Thomson <ryan@medshift.com>
2024-05-06 09:57:03 -0400

    Convert gitconfig to shell script

---
+++ gitconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git config --global alias.fetchall "fetch --all --prune"
+git config --global alias.ff "merge --ff-only"
+git config --global alias.fixup "commit --amend --no-edit"
+git config --global alias.olog "log --oneline"
+git config --global alias.shortstat "diff --shortstat"
+git config --global alias.smerge "merge --squash"
+git config --global alias.stat "diff --stat"
+
+git config --global core.pager delta
+git config --global core.excludesfile '~/.gitignore'
+
+git config --global	diff.algorithm histogram
+
+git config --global init.defaultBranch master
+
+git config --global pager.branch false
+git config --global pager.stash false
+git config --global pager.tag false
+
+git config --global pull.ff only
